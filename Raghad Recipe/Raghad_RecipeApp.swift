3999218fd3c3c805231a1d0febe8bbb5860b18ab
RaghadHassanain <145283410+RaghadHassanain@users.noreply.github.com>
2024-10-21 14:45:11 +0300

    Initial Commit

---
+++ Raghad Recipe/Raghad_RecipeApp.swift
@@ -0,0 +1,17 @@
+//
+//  Raghad_RecipeApp.swift
+//  Raghad Recipe
+//
+//  Created by Raghad Nawaf on 18/04/1446 AH.
+//
+
+import SwiftUI
+
+@main
+struct Raghad_RecipeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
